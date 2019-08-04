1f5a43b3529c2138aeece26cf19484e028908377
do02reen24 <do02reen24@gmail.com>
2019-08-04 10:48:07 +0900

    upload blog  media code

--- blogproject/urls.py
+++ blogproject/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 import blogapp.views
 import portfolio.views
 from django.conf import settings
-from django.conf.ruls.static import static
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
